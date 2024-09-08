887c5dafd7383fe254c93e107cbe6ed02766cf9d
Al-Se-La <fogot__555@outlook.com>
2024-09-08 23:05:47 +0300

    select color,button show

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log('its work');
+function oWa() {
+    window.open("https://wa.me/995571206128", "_blank");
+    // window.open("https://api.whatsapp.com/send?phone=995571206128", "_blank");
+    // window.open("https://wa.me/1234567890", "_blank");
+}
+function oIn() {
+    window.open("https://instagram.com/lavrina.lami/", "_blank");
+    // window.open("https://api.whatsapp.com/send?phone=995571206128", "_blank");
+    // window.open("https://wa.me/1234567890", "_blank");
+}
+
+
