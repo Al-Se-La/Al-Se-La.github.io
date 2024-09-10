d59f0c9eaf97b88a7f9bb4d0aa38acbba513fc16
Al-Se-La <fogot__555@outlook.com>
2024-09-11 01:13:53 +0300

    fixed contact, indent services,btn jscript

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
-function oWa() {
-    window.open("https://wa.me/995571206128", "_blank");
-    // window.open("https://api.whatsapp.com/send?phone=995571206128", "_blank");
-    // window.open("https://wa.me/1234567890", "_blank");
+let btn = document.querySelectorAll('.btn');
+let a = {
+    INSTAGRAM: "https://instagram.com/lavrina.lami/",
+    WHATSAPP:"https://wa.me/995571206128"
 }
-function oIn() {
-    window.open("https://instagram.com/lavrina.lami/", "_blank");
-    // window.open("https://api.whatsapp.com/send?phone=995571206128", "_blank");
-    // window.open("https://wa.me/1234567890", "_blank");
+// console.log(Array.from(btn));
+//console.log([...btn]);
+function openLink(v){
+    window.open(v, "_blank");
 }
+function clickOpen(e){
+    if(e.type === 'click') {
+        let target = e.target.innerText;
+        Object.keys(a).filter((k) => {
+            k === target ? openLink(a[k]) : null;
+        })
+    }
+}
+btn.forEach((button) => {
+    button.addEventListener('click', clickOpen);
+});
+//"https://wa.me/995571206128"
+//"https://api.whatsapp.com/send?phone=995571206128"
+//"https://instagram.com/lavrina.lami/"
 
 
