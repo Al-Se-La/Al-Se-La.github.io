9c2942c6d280701bf6623a84f581081de788ff53
Al-Se-La <fogot__555@outlook.com>
2024-10-11 17:58:34 +0300

    new Flex + adaptive

--- js/index.js
+++ js/index.js
@@ -1,87 +1,85 @@
-let btn = document.querySelectorAll('.btn');
-let a = {
+const btn = document.querySelectorAll('.button');
+const nav = document.querySelector('nav');
+const li = document.getElementsByClassName('menu__item');
+const img = document.getElementsByClassName('menu-icon');
+const sections = document.querySelectorAll('section');
+const menuItems = document.querySelectorAll('.menu__item');
+const courses = document.getElementById('s3');
+const imgDetails = document.getElementsByClassName('details-icon');
+const socialLinks = {
     INSTAGRAM: "https://instagram.com/lavrina.lami/",
-    WHATSAPP:"https://wa.me/995571206128"
-}
-// console.log(Array.from(btn));
-//console.log([...btn]);
-function openLink(v){
+    WHATSAPP: "https://wa.me/995571206128"
+};
+const detailsLinks = {
+    '#basic':'basic',
+    '#brow-base':'brow-base'
+};
+
+function openLink(v) {
     window.open(v, "_blank");
 }
-function clickOpen(e){
-    if(e.type === 'click') {
-        let target = e.target.innerText;
-        Object.keys(a).filter((k) => {
-            k === target ? openLink(a[k]) : null;
+
+function clickOpen(e) {
+    if (e.type === 'click') {
+        let target = e.target.innerText.toUpperCase();
+        console.log(target);
+        Object.keys(socialLinks).filter((k) => {
+            k === target ? openLink(socialLinks[k]) : null;
         })
     }
 }
-btn.forEach((button) => {
-    button.addEventListener('click', clickOpen);
+btn.forEach((v) => {
+    v.addEventListener('click', clickOpen);
 });
 //"https://wa.me/995571206128"
 //"https://api.whatsapp.com/send?phone=995571206128"
 //"https://instagram.com/lavrina.lami/"
-let nav = document.querySelector('nav');
-let ul = document.querySelector('ul');
-let li = document.querySelectorAll('li');
-let img = document.getElementsByClassName('burger_img');
-console.log(li);
-function toggleMenu(){
-    let windowWidth = window.innerWidth;
-    if(windowWidth<=510) {
-        nav.classList.toggle('hidden');
-        ul.classList.toggle('hidden_list');
-        img[0].classList.toggle('hidden_img');
-        nav.classList.contains('hidden') ? img[0].src = 'img/menu-cross.png' : img[0].src = 'img/menu-icon.png';
-    }
-}
-img[0].addEventListener('click',toggleMenu);
-[...li].forEach((item) => {
-    item.addEventListener('click',toggleMenu);
-});
-// function highlightMenuItem() {
-//         const currentHash = window.location.hash;
-//     const menuItems = document.querySelectorAll('.menu-item');
-//
-//     li.forEach(item => {
-//         console.log(item);
-//         item.classList.remove('.active');
-//     });
-// console.log(currentHash);
-//     // Находим и подсвечиваем текущую ссылку
-//     if (currentHash) {
-//         const activeItem = document.querySelector(`li a[href="${currentHash}"]`);
-//         if (activeItem) {
-//             activeItem.classList.add('active');
-//         }
-//     }
-// }
-// // Вызываем функцию при загрузке страницы
-// window.addEventListener('load', highlightMenuItem);
-// // Вызываем функцию при изменении хэша
-// window.addEventListener('hashchange', highlightMenuItem);
-// window.addEventListener('click', highlightMenuItem);
-///////////////////////////////////////////////////////////////////////////////
-const pageHi = document.getElementById('s4');
-function adjustHeight() {
-    // const windowHeight = window.visualViewport ? window.visualViewport.height : window.innerHeight;
-    // console.log(window.visualViewport.height);
-    //
-    // if (pageHi) {
-    //     // 8vh высота header, её нужно вычесть из windowHeight
-        const headerHeight = window.innerHeight * 0.08; // 8vh
-    //     pageHi.style.height = `${windowHeight - headerHeight}px`;
-    // }
-    if (!CSS.supports('height', '100dvh')) {
-        const windowHeight = window.innerHeight; // В случае отсутствия dvh, берём стандартную высоту окна
-        console.log('Window height (excluding browser UI):', windowHeight);
 
-        if (pageHi) {
-            // Устанавливаем высоту #s4 как видимая высота окна минус высота header (8vh)
-            pageHi.style.height = `${windowHeight - headerHeight}px`;
-        }
+function toggleMenu() {
+    let windowWidth = window.innerWidth;
+    if (windowWidth <= 768 && nav.style.display === 'block') {
+        detailsToggle();
+        nav.style.display = '';
+        img[0].src = 'img/menu-icon.png';
+        img[0].classList.remove('cross');
+        document.body.style.overflow = 'auto';
+    } else if (windowWidth <= 768 && nav.style.display === '') {
+        nav.style.display = 'block';
+        img[0].src = 'img/menu-cross.png';
+        img[0].classList.add('cross');
+        document.body.style.overflow = 'hidden';
     }
 }
-window.addEventListener('load', adjustHeight);
-window.addEventListener('resize', adjustHeight);
+img[0].addEventListener('click', toggleMenu);
+[...li].forEach((item) => {
+    item.addEventListener('click', toggleMenu);
+});
+
+function activateMenuItem() {
+    let index = sections.length;
+    // Проверяем положение секций относительно текущей прокрутки страницы
+    while (--index && window.scrollY + 80 < sections[index].offsetTop) {}
+    menuItems.forEach((item) => item.classList.remove('active'));
+    menuItems[index].classList.add('active');
+}
+window.addEventListener('scroll', activateMenuItem);
+
+function detailsToggle(key=false){
+    if(!key){
+    Object.values(detailsLinks).forEach((v)=>{
+        document.getElementById(v).style.display = 'none';
+    })
+    }else {
+        const details = document.getElementById(detailsLinks[key]);
+        details.style.display = 'block';
+    }
+}
+function openMore(e){
+    e.preventDefault();
+    // const detailsId = e.target.attributes[0].value.substring(1);
+     const linkHref = e.target.attributes[0].value;
+     console.log(linkHref);
+     linkHref === 'img/delete-icon.png' ? detailsToggle(): detailsToggle(linkHref);
+}
+imgDetails[0].addEventListener('click',  openMore);
+courses.addEventListener('click',openMore);
